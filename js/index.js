05fbf10d948615201852304ea7ca15b66fc2311a
Talha Khan <talha0128@gmail.com>
2019-01-01 08:55:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
 const input = document.querySelector('input');
- 
+
 input.addEventListener('keydown', function(e) {
   console.log(e.key);
 });
